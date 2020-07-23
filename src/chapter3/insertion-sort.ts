33f42ac19cb68bf39087cb73c7b11c5ef250b521
weiguo.ni <neefrankie@gmail.com>
2020-07-23 17:16:26 +0800

    doc

--- src/chapter3/insertion-sort.ts
+++ src/chapter3/insertion-sort.ts
@@ -12,11 +12,16 @@ export class InsertionSort {
   }
 
   sort() {
+    // Start from the second element of the array.
     for (let outerIndex = 1; outerIndex < this.nElems; outerIndex++) {
+      // Put the the flag element away.
       const temp = this.arr[outerIndex];
 
+      // Inner loop start from outer index, counting backward.
       let innerIndex = outerIndex;
 
+      // Counting from the element before the flag element.
+      // Walk backward to find out which element if smaller than the flag element.
       while (innerIndex > 0 && this.arr[innerIndex - 1] >= temp) {
         this.arr[innerIndex] = this.arr[innerIndex - 1];
         --innerIndex;
